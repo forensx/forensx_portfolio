bd0d89d4e1c04f3eae2767a1553bcfe873d518c5
Aniket Pant <aniketpant@jefcoed.com>
2020-05-01 18:21:44 -0500

    Fix github branch error

---
+++ forensx_portfolio/src/components/ClosingPanel.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+export default () => (
+  <div
+    style={{
+      width: "100%",
+      margin: 0,
+      padding: 0,
+      marginTop: "2%",
+      height: "100vh",
+    }}
+  >
+    <div
+      className="sectionHeader"
+      style={{
+        fontWeight: "bold",
+        fontSize: "80px",
+        marginLeft: "20%",
+        width: "60%",
+        lineHeight: "100px",
+      }}
+    >
+      We’re always looking to learn something new.
+    </div>
+    <div
+      class="closingGrid"
+      style={{
+        marginLeft: "20%",
+        marginTop: "4%",
+        marginRight: "20%",
+        width: "60%",
+      }}
+    >
+      <div class="projectEmail" style={{ fontSize: "40px" }}>
+        <div className="projectDirective">
+          Got a project?<br></br>Let's talk.
+        </div>
+        <div className="contactEmail" style={{ marginTop: "20%" }}>
+          Drop us a line at
+          <div style={{ color: "#920A0F", fontWeight: "bold" }}>
+            contact@forensx.com
+          </div>
+        </div>
+      </div>
+      <div class="projectFormEntry" style={{ fontSize: "40px" }}>
+        Or, let us know here.
+      </div>
+    </div>
+  </div>
+)
---
+++ forensx_portfolio/src/components/header.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default () => (
+  <div
+    style={{
+      width: "100%",
+      height: "100px",
+      margin: 0,
+      padding: 0,
+      background: "#F0F0F0",
+    }}
+  >
+    <img
+      src="../imgs/forensx.png"
+      alt="FORENSX LOGO HERE"
+      style={{ marginTop: "2%", marginLeft: "5%" }}
+    />
+  </div>
+)
