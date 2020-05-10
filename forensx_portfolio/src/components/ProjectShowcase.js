73c20902b4a4bb236130fb96a8a9c673057d9f6e
Aniket Pant <aniketpant@jefcoed.com>
2020-05-10 17:24:54 -0500

    Populating projects panel

--- forensx_portfolio/src/components/ProjectShowcase.js
+++ forensx_portfolio/src/components/ProjectShowcase.js
@@ -1,12 +1,14 @@
 import React from "react"
 
+import FRI_panel from "../images/FRI_panel.png"
+
 export default () => (
   <div
     style={{
       width: "100%",
       margin: 0,
       padding: 0,
-      height: "100vh",
+      // height: "70vh",
     }}
   >
     <div
@@ -28,14 +30,94 @@ export default () => (
         marginTop: "2%",
         marginRight: "20%",
         width: "60%",
-        height: "500px",
+        // height: "500px",
       }}
     >
-      <div className="projectFRI projectCard">FRI</div>
-      <div className="projectGViz projectCard">GViz</div>
-      <div className="projectSciSearch projectCard">Sci-Search</div>
-      <div className="projectTanzania projectCard">Tanzania</div>
-      <div className="projectNYC projectCard">Magic City NYC</div>
+      <div className="projectFRI projectCard">
+        <div class="container">
+          <img src={FRI_panel} alt="Avatar" className="projectImage"></img>
+          <div class="overlay">
+            <div class="overlayText">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projectLink"
+                href="https://2019.spaceappschallenge.org/challenges/living-our-world/curious-minds-come-helping-hands/teams/forensx/project"
+              >
+                Famine Risk Index
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="projectGViz projectCard">
+        <div class="container">
+          <img src={FRI_panel} alt="Avatar" className="projectImage"></img>
+          <div class="overlay">
+            <div class="overlayText">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projectLink"
+                href="https://devpost.com/software/visualizing-overlapping-data-between-bed-files"
+              >
+                GViz
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="projectSciSearch projectCard">
+        <div class="container">
+          <img src={FRI_panel} alt="Avatar" className="projectImage"></img>
+          <div class="overlay">
+            <div class="overlayText">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projectLink"
+                href="https://devpost.com/software/sci-search-7s9xqb"
+              >
+                Sci-Search
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="projectTanzania projectCard">
+        <div class="container">
+          <img src={FRI_panel} alt="Avatar" className="projectImage"></img>
+          <div class="overlay">
+            <div class="overlayText">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projectLink"
+                href="https://www.congressionalappchallenge.us/2018-al6/"
+              >
+                Tanzania
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="projectNYC projectCard">
+        <div class="container">
+          <img src={FRI_panel} alt="Avatar" className="projectImage"></img>
+          <div class="overlay">
+            <div class="overlayText">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="projectLink"
+                href="https://2019.spaceappschallenge.org/challenges/living-our-world/curious-minds-come-helping-hands/teams/forensx/project"
+              >
+                Magic City Hacks
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 )
